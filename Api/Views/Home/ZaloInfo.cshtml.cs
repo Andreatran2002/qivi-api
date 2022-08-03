93b4b7887bf2ac850c4727d98122808bb100aac3
andreatran2002 <andreatran2002@gmail.com>
2022-08-03 15:12:11 +0700

    udpate

---
+++ Api/Views/Home/ZaloInfo.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZaloDotNetSDK;
+
+namespace Api.Views.Home
+{
+	public class ZaloInfoModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+
+            //ZaloAppInfo appInfo = new ZaloAppInfo(2088490447091653618, "S5Yq6BT2OVY1IDmRG8E8", "callbackUrl");
+            //Za/loAppClient appClient = new ZaloAppClient(appInfo);
+        }
+    }
+}
